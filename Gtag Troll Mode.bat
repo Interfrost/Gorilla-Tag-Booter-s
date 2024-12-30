387b8ed9869938fe67fa09f4973c68b86086b016
Interfrost <interfrost.real@gmail.com>
2024-12-29 22:27:06 -0600

    Add files via upload

---
+++ Gtag Troll Mode.bat
@@ -0,0 +1,6 @@
+@echo off
+Start:
+Start "C:\Program Files\Steam\steamapps\common\Gorilla Tag"
+Start "Soundboard/Soundboards (File Directory)"
+Start "Menu Loader | If .DLL Delete This Line(File Directory)"
+Start "MMM (File Directory) (Optional)"
